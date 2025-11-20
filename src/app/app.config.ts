40fe3b2e47549b292a22bc5d71fb249d6ab32741
Ashot Markosyan <a.markosyan@voxloud.com>
2025-11-20 19:29:04 +0400

    installed angular and configured ui lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,43 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 import { routes } from './app.routes';
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        colorScheme: {
+            light: {
+                semantic: {
+                    highlight: {
+                        background: '{primary.50}',
+                        color: '{primary.700}',
+                    }
+                }
+            },
+            dark: {
+                semantic: {
+                    highlight: {
+                        background: '{primary.200}',
+                        color: '{primary.900}',
+                    }
+                }
+            }
+        }
+    }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } }
+    })
   ]
 };
